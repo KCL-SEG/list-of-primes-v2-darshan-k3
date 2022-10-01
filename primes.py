ba5b0f1c1cb2bff7103c00921c26098c42de7cc2
darshan-k3 <96890748+darshan-k3@users.noreply.github.com>
2022-10-01 15:55:48 +0100

    raise valueError

--- primes.py
+++ primes.py
@@ -1,6 +1,23 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
-    list = []
-    return list
+    if(number_of_primes <= 0):
+        raise ValueError
+    n = 0
+    i = 0
+    primes = []
+
+    while n <= number_of_primes:
+        i += 1
+        count = 1
+        for j in range(2, i):
+            if i % j == 0:
+                count = 0
+                break
+        if count == 1:
+            primes.append(i)
+            n += 1
+    # to get rid of 1 at the start
+    return primes[1:]
